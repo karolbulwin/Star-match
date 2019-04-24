0b9fff44c257f727c2d5a1280602163dd5268288
Karol Bulwin <karolbulwin@gmail.com>
2019-04-24 17:24:31 +0200

    new element - message

--- src/App.js
+++ src/App.js
@@ -23,6 +23,11 @@ const PlayButton = props => (
 
 const PlayAgain = props => (
   <div className='game-done'>
+  <div className='message'
+    style={{ color: props.gameStatus === 'lost' ? 'red' : 'green'}}
+  >
+    {props.gameStatus === 'lost' ? 'Game Over' : 'Nice'}
+  </div>
     <button onClick={props.onClick}>Play Again</button>
   </div>
 );
