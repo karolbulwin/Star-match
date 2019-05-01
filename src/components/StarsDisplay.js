c6304a8c4ec8768d591fbed2f82707bf53156d09
Karol Bulwin <karolbulwin@gmail.com>
2019-05-01 15:32:46 +0200

    new class

--- src/components/StarsDisplay.js
+++ src/components/StarsDisplay.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { utils } from "../utils";
 export const StarsDisplay = props => (
-  <>
+  <div className="stars-container">
     {utils.range(1, props.count).map(startId => (
       <div key={startId} className="star" />
     ))}
-  </>
+  </div>
 );
