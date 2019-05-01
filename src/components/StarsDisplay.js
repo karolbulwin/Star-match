4f2e483438c5aa3313e350ef45e2f7fc2e5e4ccd
Karol Bulwin <karolbulwin@gmail.com>
2019-05-01 20:38:22 +0200

    react logo instead of stars

--- src/components/StarsDisplay.js
+++ src/components/StarsDisplay.js
@@ -1,9 +1,13 @@
 import React from "react";
+import logo from "../logo.svg";
+
 import { utils } from "../utils";
 export const StarsDisplay = props => (
   <div className="stars-container">
     {utils.range(1, props.count).map(startId => (
-      <div key={startId} className="star" />
+      <div key={startId} className="star">
+        <img src={logo} className="App-logo" alt="logo" />
+      </div>
     ))}
   </div>
 );
