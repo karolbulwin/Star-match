e16abbcf0abdc8f1885781c9c472b2d59043440a
Karol Bulwin <karolbulwin@gmail.com>
2019-05-02 12:38:03 +0200

    Updates

---
+++ precache-manifest.5c4f3e1132dfe70ce5431eaef58c5098.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d7e0ded6020f47c2949ff9ccfb947c56",
+    "url": "/Star-match/index.html"
+  },
+  {
+    "revision": "4fbf4174e468c28b6ccb",
+    "url": "/Star-match/static/css/main.35fe1f04.chunk.css"
+  },
+  {
+    "revision": "cb3172cd3b50fb1f7f2b",
+    "url": "/Star-match/static/js/2.2b91e2f1.chunk.js"
+  },
+  {
+    "revision": "4fbf4174e468c28b6ccb",
+    "url": "/Star-match/static/js/main.fcaea721.chunk.js"
+  },
+  {
+    "revision": "a681437573ddac169f0a",
+    "url": "/Star-match/static/js/runtime~main.7a5dc9f7.js"
+  },
+  {
+    "revision": "bfd7af7e54b5a817966fef7e5a145745",
+    "url": "/Star-match/static/media/star.bfd7af7e.svg"
+  }
+]);
