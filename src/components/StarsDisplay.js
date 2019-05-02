3e6e07ac05cf3cf35b74dfd223e8e4bb5cc91ae1
Karol Bulwin <karolbulwin@gmail.com>
2019-05-02 12:01:53 +0200

    png for older browsers

--- src/components/StarsDisplay.js
+++ src/components/StarsDisplay.js
@@ -1,12 +1,13 @@
 import React from "react";
-import start from "../images/star.svg";
+import star from "../images/star.svg";
+import starPNG from "../images/star.png";
 
 import { utils } from "../utils";
 export const StarsDisplay = props => (
   <div className="stars-container">
     {utils.range(1, props.count).map(startId => (
       <div key={startId} className="star">
-        <img src={start} className="App-star" alt="" />
+        <img src={starPNG} srcSet={star} className="App-star" alt="" />
       </div>
     ))}
   </div>
