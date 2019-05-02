75782a2a9aaf8142775ae173c0036018c1caf098
Karol Bulwin <karolbulwin@gmail.com>
2019-05-02 11:56:36 +0200

    rename

--- src/components/StarsDisplay.js
+++ src/components/StarsDisplay.js
@@ -6,7 +6,7 @@ export const StarsDisplay = props => (
   <div className="stars-container">
     {utils.range(1, props.count).map(startId => (
       <div key={startId} className="star">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={start} className="App-star" alt="" />
       </div>
     ))}
   </div>
